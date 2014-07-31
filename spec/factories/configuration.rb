a60abe7eadcce7ff737798148d53ef7b6a058328
junhanamaki <jun.hanamaki@gmail.com>
2014-07-31 15:29:47 +0100

    removing unnecessary code

--- spec/factories/configuration.rb
+++ spec/factories/configuration.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :configuration, class: Rekiq::Configuration do
-    shift { [*-100..100].sample }
-    schedule_post_work { [true, false].sample }
-    expiration_margin  { [*0..100].sample }
-    schedule_expired   { [true, false].sample }
+    shift              [*-100..100].sample
+    schedule_post_work [true, false].sample
+    expiration_margin  [*0..100].sample
+    schedule_expired   [true, false].sample
   end
 end
